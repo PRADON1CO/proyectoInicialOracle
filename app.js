b1bc5dafef02c7f68e8efc7cba92c4943b60cc3f
Prado Nicolas <nicolas88prado@gmail.com>
2025-01-23 18:44:52 -0300

    le agregue condicionales para poder aplicar el uso del dom y bom

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-alert("Hola Muchachos");
+const numeroSecreto = 6;
+let nuemeroUsuario = prompt("Me puedes indicar un número por favor: ");
+
+console.log(nuemeroUsuario);
+
+const mensajeElemento = document.getElementById("mensaje");
+
+if (nuemeroUsuario == numeroSecreto) {
+  mensajeElemento.innerHTML = `
+        <h1><span class="container__texto-azul">Correcto!</span></h1>
+        <h2>Descubriste el número secreto!</h2>
+    `;
+} else {
+  mensajeElemento.innerHTML = `
+        <h1 class="container__texto-azul">Lo siento</h1>
+        <h2>El número secreto era ${numeroSecreto}</h2>
+    `;
+}
